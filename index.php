d58594a46d3c2ed4fb37a3742a1faa0a5eccf38b
Ean Daus <eandaus123@gmail.com>
2019-02-11 16:55:08 -0500

    Inserted data from a pdo statement

--- index.php
+++ index.php
@@ -5,13 +5,39 @@
  * index.php
  * snow day PDO activity
  */
+//php error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 //Connect to DB
-require '/home/edausgre_grc/config.php';
+require '/home/edausgre/config.php';
+
 try{
     //instantiate a new database object
     $dbh = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
     echo 'Connected to Database!';
+
 }catch(PDOException $e) {
     echo $e->getMessage();
 }
+
+//Define the query
+$sql = "INSERT INTO pets(type, name, color)
+        VALUES (:type, :name, :color)";
+
+//Prepare the statement
+$statement = $dbh->prepare($sql);
+
+//Bind the params
+$type = 'kangaroo';
+$name = 'Joey';
+$color = 'purple';
+$statement->bindParam(':type', $type, PDO::PARAM_STR);
+$statement->bindParam(':name', $name, PDO::PARAM_STR);
+$statement->bindParam(':color', $color, PDO::PARAM_STR);
+
+//Execute
+$statement->execute();
+echo 'executed';
+
+//Process the result
